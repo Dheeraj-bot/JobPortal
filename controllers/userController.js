b229955332087f3ee8bfd27a0d34ff9a45edc24a
DheerajNani <136476062+Dheeraj-bot@users.noreply.github.com>
2025-02-03 00:57:29 +0530

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -102,6 +102,8 @@ export const login = catchAsyncErrors(async (req, res, next) => {
       .cookie("token", "", {
         expires: new Date(Date.now()),
         httpOnly: true,
+      secure: true,
+      sameSite: "None"
       })
       .json({
         success: true,
